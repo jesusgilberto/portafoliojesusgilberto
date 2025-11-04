762cc43a0bb564b12efc85f7a9dd6cfb33381f10
JESUS GILBERTO RAMIREZ LIZARRAGA <gilbertoramirez@solemti.com>
2025-11-04 11:46:11 -0700

    feat: agregado el componente About y realizadas modificaciones en el archivo app.ts

--- src/app/app.ts
+++ src/app/app.ts
@@ -1,13 +1,17 @@
 import { Component } from '@angular/core';
 import { RouterOutlet } from '@angular/router';
 import { HeaderComponent } from "./components/header/header";
+import { AboutComponent } from "./components/about/about";
+import { FooterComponent } from "./components/footer/footer";
 
 @Component({
   selector: 'app-root',
   standalone: true,
   imports: [
     HeaderComponent,
-  ],
+    AboutComponent,
+    FooterComponent
+],
   templateUrl: './app.html',
   styleUrl: './app.scss'
 })
